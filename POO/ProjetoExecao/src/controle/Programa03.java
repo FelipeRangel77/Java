c05f2fa070efb0fc5181d414e2f0ae92a3275429
FelipeRangel77 <felipealbuquerquerangel@gmail.com>
2026-02-11 18:51:20 -0300

    update

--- POO/ProjetoExecao/src/controle/Programa03.java
+++ POO/ProjetoExecao/src/controle/Programa03.java
@@ -1,5 +1,21 @@
 package controle;
 
-public class Programa03 {
+import java.util.Scanner;
 
+public class Programa03 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String[] nomes = {"Maria", "Mariana", "Gabriel", "Tainá", "Fernanda", "Ana"};
+		
+		System.out.print("Digite um número de 0 a 5: ");
+		int num = input.nextInt();
+		
+		try {
+			System.out.print("Pessoa consultada: " + nomes[num]);
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Erro: " + e.getMessage());
+			System.out.println("O número deve ser entre 0 e 5");
+		}
+	}
 }
--- POO/ProjetoExecao/src/controle/Programa06.java
+++ POO/ProjetoExecao/src/controle/Programa06.java
@@ -1,5 +1,20 @@
 package controle;
 
-public class Programa06 {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
+public class Programa06 {
+	public static void main(String[] args) {
+		String caminho = "C:/Users/26012058/Documents/arquivo.txt";
+		
+		try {
+			FileInputStream arquivo = new FileInputStream(caminho);
+			System.out.println("Arquivo existe no disco!");
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("Arquivo não existe no disco");
+			System.out.println("Erro: " + e.getMessage());
+		}
+		
+	}
 }
